e7776125ece937f848a230bcddd45842288dc163
Eric McGuire <radiotraks@users.noreply.github.com>
2013-10-09 09:09:46 -0400

    Update html5audio.js

--- www/js/html5audio.js
+++ www/js/html5audio.js
@@ -1,6 +1,5 @@
 
 
-
 var progressTimer;
 
 var playButton;
@@ -23,7 +22,7 @@ function pad2(number) {
 	return (number < 10 ? '0' : '') + number
 }
 
-var myaudioURL = 'http://stream.4zzzfm.org.au:789/';
+var myaudioURL = 'http://icy3.abacast.com/nabco-wrkzfmmp3-48.m3u';
 var myaudio = new Audio(myaudioURL);
 var isPlaying = false;
 var readyStateInterval = null;
